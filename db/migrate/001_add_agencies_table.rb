1df36791ddfb0c431fe21d59d69a60508063e324
MacPapo <jacopocostantini32@gmail.com>
2023-11-13 00:50:58 +0100

    TimeZone function added to the first migration

--- db/migrate/001_add_agencies_table.rb
+++ db/migrate/001_add_agencies_table.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 Sequel.migration do
-  change do
-    create_table :agencies do
-      String   :agency_id,       primary_key: true
-      String   :agency_name,     null: false
-      String   :agency_url,      null: false
-      String   :agency_timezone, null: false
-      String   :agency_lang
-      String   :agency_phone
-      String   :agency_fare_url
-      String   :agency_email
+  up do
+    run <<-SQL
+CREATE OR REPLACE FUNCTION is_timezone(
+    tz TEXT
+)
+RETURNS BOOLEAN AS $$
+DECLARE
+    date TIMESTAMPTZ;
+BEGIN
+    date := now() AT TIME ZONE tz;
+    RETURN TRUE;
+EXCEPTION WHEN invalid_parameter_value THEN
+    RETURN FALSE;
+END;
+$$ LANGUAGE plpgsql STABLE;
+SQL
 
-      check { Sequel.function(:is_timezone, :agency_timezone) }
+    change do
+      create_table :agencies do
+        String   :agency_id,       primary_key: true
+        String   :agency_name,     null: false
+        String   :agency_url,      null: false
+        String   :agency_timezone, null: false
+        String   :agency_lang
+        String   :agency_phone
+        String   :agency_fare_url
+        String   :agency_email
+
+        check { Sequel.function(:is_timezone, :agency_timezone) }
+      end
     end
   end
 end

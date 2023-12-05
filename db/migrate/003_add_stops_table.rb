3e2c66b6303119c3dc2ab343f8b9785a5f40931a
MacPapo <jacopocostantini32@gmail.com>
2023-12-05 15:24:55 +0100

    Added migration for new clustering algo.

--- db/migrate/003_add_stops_table.rb
+++ db/migrate/003_add_stops_table.rb
@@ -2,23 +2,17 @@
 
 Sequel.migration do
   change do
-    create_table(:stops) do
+    create_table(:stop_clusters) do
       primary_key      :id
 
-      Integer          :stop_id,        null: false
-      String           :stop_code
-      String           :stop_name,      null: false
-      Float            :stop_lat,       null: false
-      Float            :stop_lon,       null: false
-      String           :stop_category,  null: false
-      String           :parent_station
+      Integer          :cluster_name,        null: false
+      Float            :cluster_lat,       null: false
+      Float            :cluster_lon,       null: false
 
-      index :stop_id
-      index :stop_name
-      index :stop_lat
-      index :stop_lon
-      index :stop_category
-      index :parent_station
+      index :id
+      index :cluster_name
+      index :cluster_lat
+      index :cluster_lon
     end
   end
 end
---
+++ db/migrate/004_add_stops_table.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table(:stops) do
+      primary_key      :id
+
+      Integer          :stop_id,        null: false
+      String           :stop_code
+      String           :stop_name,      null: false
+      Float            :stop_lat,       null: false
+      Float            :stop_lon,       null: false
+      String           :stop_category,  null: false
+      String           :parent_station
+      Integer          :cluster_id,     null: false
+
+      index :stop_id
+      index :stop_name
+      index :stop_lat
+      index :stop_lon
+      index :stop_category
+      index :parent_station
+      index :cluster_id
+    end
+  end
+end

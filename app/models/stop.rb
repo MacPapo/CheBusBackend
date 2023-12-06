441842dc97675f6f2908f3a56d4a35933c7f0cd4
MacPapo <jacopocostantini32@gmail.com>
2023-12-06 15:48:30 +0100

    Fixed Cluster Model

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -3,5 +3,15 @@
 module Models
   # Stops Table
   class Stop < Sequel::Model
+    many_to_one :stop_clusters, key: :cluster_id
+    
+    ALL_STOPS_NO_CLUSTER = 
+      select(:stop_name, :stop_lat, :stop_lon).
+      where(cluster_id: nil).
+      prepare(:all, :give_all_stops_no_cluster)
+    
+    def self.give_all_stops_no_cluster()
+      ALL_STOPS_NO_CLUSTER.call
+    end
   end
 end
